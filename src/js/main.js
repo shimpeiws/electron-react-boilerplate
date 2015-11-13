518252ce1c5802570e8fd587e114a73cc1bcfc82
Shimpei Takamatsu <takamatsu@enigmo.co.jp>
2015-11-13 19:06:27 +0900

    add App

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
-let a = 'aaa';
+'use strict';
 
-if (a === 'aaa') {
-    true;
-}
+import app from 'app';
+import BrowserWindow from 'browser-window';
+import CrashReporter from 'crash-reporter';
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    if(mainWindow) {
+        return;
+    }
+    
+    mainWindow.loadUrl('file://' + __dirname + '/timeline.html');
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
+});
